4dca8a2ba1ed1e7cc2d98162326461033b5c002d
Abeltadesse19 <tadesseabel19@gmail.com>
2022-12-15 13:35:32 -0800

    getline

--- getline.c
+++ getline.c
@@ -159,6 +159,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
 /**
  * sigintHandler - blocks ctrl-C
  * @sig_num: signal number
+ *
  * Return: void
  */
 void sigintHandler(__attribute__((unused))int sig_num)
