3e493276348e6d448f7ffb676f0dbb2c9133cfc0
Abeltadesse19 <tadesseabel19@gmail.com>
2022-12-19 15:21:04 -0800

    update builtin1

--- builtin1.c
+++ builtin1.c
@@ -58,7 +58,7 @@ int set_alias(info_t *info, char *str)
 		return (unset_alias(info, str));
 
 	unset_alias(info, str);
-	return (add_node_end(&(info->alias), str, o) == NULL);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
 
 /**
